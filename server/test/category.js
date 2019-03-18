9d3baf0d156a47776bf9e7ada2b3959c27620103
Jesus Emilio Lombardo <jesus.lombardo@globant.com>
2019-03-18 12:23:25 -0300

    t13

--- server/test/category.js
+++ server/test/category.js
@@ -7,8 +7,8 @@ const should = chai.should();
 chai.use(chaiHttp)
 
 describe('/GET category', function() {
-    it('it should Get all categories', async (done) => {
-        await chai.request(app)
+    it('it should Get all categories', (done) => {
+         chai.request(app)
         .get('/categories')
         .end((err, res) => {
             res.should.have.status(200);

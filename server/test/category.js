5aa8e10933e5ae4d429a81cc00dcd0777de6a3e5
Jesus Emilio Lombardo <jesus.lombardo@globant.com>
2019-03-18 12:29:35 -0300

    t14

--- server/test/category.js
+++ server/test/category.js
@@ -1,14 +1,18 @@
+process.env.NODE_ENV = "test"
+
 const Category = require('../models').Category;
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const app = require('../../app');
 
 const should = chai.should();
+
 chai.use(chaiHttp)
 
-describe('/GET category', function() {
-    it('it should Get all categories', (done) => {
-         chai.request(app)
+
+describe('/GET category', () => {
+    it('it should Get all category', (done) => {
+        chai.request(app)
         .get('/categories')
         .end((err, res) => {
             res.should.have.status(200);

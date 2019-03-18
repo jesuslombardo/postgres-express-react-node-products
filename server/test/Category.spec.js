6dec08c4032dce13f87c466720af90c539b77096
Jesus Emilio Lombardo <jesus.lombardo@globant.com>
2019-03-18 16:47:20 -0300

    t27

--- server/test/Category.spec.js
+++ server/test/Category.spec.js
@@ -24,50 +24,26 @@ const {
         ;['name'].forEach(checkPropertyExists(instance))
         })
     
-
-        /*
-        context('check associations', () => {
-            it("defined a belongsToMany association with Category through CategoryProduct as 'categories'", () => {
-                expect(Category.belongsToMany).to.have.been.calledWith(Product, {
-                    through: CategoryProduct,
-                    as: 'products'
-                })
-            })
-        })
-        
-        context('check associations', () => {
-            const OtherModel = 'Product'; // it doesn't matter what
-            before(() => {
-            Model.associate({ OtherModel })
-            })
-            it('defined a belongsTo association with OtherModel', () => {
-            expect(Category.belongsToMany).to.have.been.calledWith(OtherModel)
-            })
-        })
-        */
     })
+
+
 
 
     //const Category = require('../models').Category;
     const chai = require('chai');
     const chaiHttp = require('chai-http');
-    const app = require('../../app');
     chai.use(chaiHttp);
-    
-    
+    const app = require('../../bin/www');
 
-    const should = require('should');
-
-    
-
-
-
-it('fails, as expected', function(done) { // <= Pass in done callback
-    chai.request(app)
-    .get('/categories')
-    .end(function(err, res) {
-      expect(res).to.have.status(200);
-      done();                               // <= Call done to signal callback end
+    it('list of all categories', function(done) { // <= Pass in done callback
+        chai.request(app)
+        //chai.request('https://backend-test-p5.herokuapp.com')
+        .get('/categories')
+        .end(function(err, res) {
+            expect(res).to.have.status(200);
+            expect(res.body).to.be.an('array');
+            done();// <= Call done to signal callback end
+        });
     });
-  });
+
 

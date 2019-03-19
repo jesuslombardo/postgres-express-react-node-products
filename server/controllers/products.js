a30ab198bbc89a58c45c2a625cabddc2539228f5
Jesus Emilio Lombardo <jesus.lombardo@globant.com>
2019-03-19 14:40:47 -0300

    better association

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -34,7 +34,7 @@ module.exports = {
 
     return Product
       .findAll({
-        include: [{
+        /*include: [{
           model: Category,
           as: 'categories',
           required: true, //filtra
@@ -46,7 +46,7 @@ module.exports = {
             attributes: []
           }
           }],
-          attributes: ['name','price', 'description','available'],
+          attributes: ['name','price', 'description','available'],*/
         })
       .then((products) => res.status(200).send(products))
       .catch((error) => res.status(400).send(error));
